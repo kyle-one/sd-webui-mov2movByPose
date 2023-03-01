0d95f4d6cbd0c526434ca21785b3e3d64c3283ee
Kyle <kyle993841271@gmail.com>
2023-03-01 20:06:41 +0800

    test

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 import launch
 
-print('Installing requirements for Mov2mov')
+print('Installing requirements for Mov2movByPose')
 if not launch.is_installed("cv2"):
     launch.run_pip("install opencv-python", "requirements for opencv")
